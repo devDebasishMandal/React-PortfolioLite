6039a83692d1603286d73a697bac515f4b634b2c
devDebasishMandal <debasishmandal186@gmail.com>
2024-01-18 02:24:41 +0530

    optimized the code by using map to render redundent data

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,33 @@ import About from "./About";
 import Section from "./Section";
 import Footer from "./Footer";
 
-function App(){
-    return(<div>
-        <Navbar/>
-        <About/>
-        <Section name="Skills" para="ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quas cupiditate voluptatum soluta ab autem atque corrupti itaque architecto, nobis assumenda enim ipsum doloremque unde. Itaque alias "/>
-        <Section name="Education" para="ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quas cupiditate voluptatum soluta
-        ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quas cupiditate voluptatum soluta ab autem atque corrupti itaque architecto, nobis assumenda enim ipsum doloremque unde. Itaque ab autem atque corrupti itaque architecto, nobis assumenda enim ipsum doloremque unde. Itaque alias "/>
-        <Section name="Projects" para="ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quas cupiditate voluptatum soluta ab autem atque corrupti itaque architecto, nobis assumenda enim ipsum doloremque unde. Itaque
-        ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quas cupiditate voluptatum soluta ab autem atque corrupti itaque architecto, nobis assumenda enim ipsum doloremque unde. Itaque alias "/>
-        <Footer/>
-        
-    </div>)
+const sectionData = [
+  {
+    name: "Skills",
+    para: "ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quas cupiditate voluptatum soluta ab autem atque corrupti itaque architecto, nobis assumenda enim ipsum doloremque unde. Itaque alias ",
+  },
+  {
+    name: "Education",
+    para: "ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quas cupiditate voluptatum soluta ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quas cupiditate voluptatum soluta ab autem atque corrupti itaque architecto, nobis assumenda enim ipsum doloremque unde. Itaque ab autem atque corrupti itaque architecto, nobis assumenda enim ipsum doloremque unde. Itaque alias ",
+  },
+  {
+    name: "Projects",
+    para: "ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quas cupiditate voluptatum soluta ab autem atque corrupti itaque architecto, nobis assumenda enim ipsum doloremque unde. Itaque ipsum dolor sit amet consectetur adipisicing elit. Veritatis quas cupiditate voluptatum soluta ab autem atque corrupti itaque architecto, nobis assumenda enim ipsum doloremque unde. Itaque alias ",
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <About />
+      {sectionData.map((e, i) => {
+       return <Section name={e.name} para={e.para} />;
+      }) 
+      }
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
-
-
